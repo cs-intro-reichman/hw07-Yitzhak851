a602925f45740c3880ac05399bf14be8990a04be
YBO <yitzhak851@gmail.com>
2024-01-30 11:34:33 +0200

    Delete class file

--- LevenshteinTester.java
+++ LevenshteinTester.java
@@ -5,12 +5,9 @@ public class LevenshteinTester {
             System.out.println("Usage: java LevenshteinTester <word1> <word2>");
             return;
         }
-
         String word1 = args[0];
         String word2 = args[1];
-
         int distance = SpellChecker.levenshtein(word1, word2);
-
         System.out.println("Levenshtein Distance between " + word1 + " and " + word2 + ": " + distance);
     }
 }
